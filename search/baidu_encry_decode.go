8dc62baac87b8001b77751893cf763a188837c54
kevin-zx <zhangxian887@gmail.com>
2020-01-21 16:48:50 +0800

    body有可能会被读取了，所以html也要返回

--- search/baidu_encry_decode.go
+++ search/baidu_encry_decode.go
@@ -9,17 +9,17 @@ import (
 )
 
 func DecodeBaiduEncURL(baiduUrl string) string {
-	response, err := GetWebconAndRealUrlFromBaiduUrl(baiduUrl)
+	response, _, err := GetWebconAndRealUrlFromBaiduUrl(baiduUrl)
 	if err != nil {
 		return ""
 	}
 	return response.Request.URL.String()
 }
 
-func GetWebconAndRealUrlFromBaiduUrl(baiduUrl string) (response *http.Response, err error) {
+func GetWebconAndRealUrlFromBaiduUrl(baiduUrl string) (response *http.Response, webCon string, err error) {
 	response, err = http_util.SendRequest(baiduUrl, map[string]string{"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"}, "GET", nil, 10*time.Second)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	//fmt.Println(baiduUrl)
 	realURL := ""
@@ -28,7 +28,7 @@ func GetWebconAndRealUrlFromBaiduUrl(baiduUrl string) (response *http.Response, 
 		if strings.Contains(h, "window.opener&&window.opener.bds&&window.opener.bds.pdc&&window.opener.bds.pdc.sendLinkLog") {
 			part1 := strings.Split(h, "window.location.replace(\"")
 			if len(part1) < 2 {
-				return nil, errors.New("can't go real page")
+				return nil, "", errors.New("can't go real page")
 			} else {
 				realURL = strings.Split(part1[1], "\")},timeout")[0]
 			}
@@ -50,9 +50,17 @@ func GetWebconAndRealUrlFromBaiduUrl(baiduUrl string) (response *http.Response, 
 		if realURL == "" {
 			return
 		} else {
-			return http_util.GetWebResponseFromUrlWithHeader(realURL, map[string]string{"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"})
+			response, err = http_util.GetWebResponseFromUrlWithHeader(realURL, map[string]string{"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"})
+			if err != nil {
+				return
+			}
+			webCon, err = http_util.ReadContentFromResponse(response, "")
+			if err != nil {
+				return
+			}
+			return
 		}
 
 	}
-	return nil, errors.New("wrong status")
+	return nil, "", errors.New("wrong status")
 }
